75ad98c4bf8c21a0183a83d1d5d56789d8b11305
User <memmedlinihad103@gmail.com>
2024-10-18 22:35:33 +0300

    Second Commit

--- app/src/main/java/com/example/test9/MainActivity.kt
+++ app/src/main/java/com/example/test9/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.test9
 
 import android.app.Activity
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.os.Message
 import android.view.LayoutInflater
+import android.view.Window
+import android.widget.Button
 import android.widget.TableRow
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -38,5 +45,34 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        binding.logOut.setOnClickListener {
+
+            val message: String? = "Are you sure you want to log out"
+            showAlertDialog(message)
+        }
+    }
+
+
+    fun showAlertDialog(message: String?) {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.alert_dialog)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val alertMessage = dialog.findViewById<TextView>(R.id.alert_message)
+        val alertYesBtn = dialog.findViewById<Button>(R.id.alertYesBtn)
+        val alertNoBtn = dialog.findViewById<Button>(R.id.alertNoBtn)
+
+        alertMessage.text = message
+
+        alertYesBtn.setOnClickListener {
+            Toast.makeText(this, "Button Yes Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        alertNoBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
